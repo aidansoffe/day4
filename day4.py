836a102f1ee4953293998c0780b8fd5d7c36d922
Moon Munar <moonmunar@Moons-MacBook-Pro.local>
2018-11-19 16:50:20 -0600

    my day 4

---
+++ day4.py
@@ -0,0 +1,20 @@
+name="Kim"
+age="22"
+print("Her name is " + name + " and she is " + age + " years old.")
+
+phrase="Munar nails"
+print(phrase)
+
+print(phrase + " is a good salon")
+
+print(phrase.lower())
+
+print(phrase.upper())
+
+print(len(phrase))
+
+
+print(phrase[3])
+
+print(phrase.index("l"))
+print(phrase.replace ("Munar", "Beck"))
